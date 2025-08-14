e3c2e3c33c4b1d3c54eeee5882e15e8c970519a7
강서윤 <20231054@sungshin.ac.kr>
2025-08-14 17:21:57 +0900

    캡챠제작중

--- captcha_stage/urls.py
+++ captcha_stage/urls.py
@@ -1,8 +1,9 @@
+# captcha_stage/urls.py
 from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index, name='captcha_index'),
+    path('', views.index, name='stage1'),
     path('captcha-image/', views.generate_captcha, name='captcha_image'),
     path('check-captcha/', views.check_captcha, name='check_captcha'),
 ]
